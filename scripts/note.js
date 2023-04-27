17cabf903c7b1aece9c037bb9a7d39f41663173a
katrihumaloja <n1huka00@students.oamk.fi>
2023-04-27 21:15:12 +0300

    Update

--- scripts/note.js
+++ scripts/note.js
@@ -1,17 +1,23 @@
+// Katri Humaloja
+
+// Tulostetaan viesti sivulle
 function addNote() {
+    // Määritellään muuttujat
     let name = document.getElementById("name").value;
     let message = document.getElementById("message").value;
     let cardBody = document.querySelector(".card-body");
     let checkbox = document.getElementById("important");
-    let currentDate = new Date();
-    let dateString = currentDate.toDateString();
+    let currentDate = new Date().toLocaleDateString();
 
+    // Sininen taustaväri viestille
     messageHtml = '<div class="alert alert-info';
 
+    // Jos checkbox on valittu, viestin taustaväri on punainen
     if (checkbox.checked == true) {
         messageHtml += ' alert-danger';
     }
 
-    messageHtml += '">' + dateString +  ' ' + '<strong>' + name + '<br>' + message + '</div>';
+    // Tulostetaan päivä, nimi ja viesti sivulle
+    messageHtml += '">' + '<strong>' + currentDate +  ' ' + name + '</strong>' + '<br>' + message + '</div>';
     cardBody.innerHTML = cardBody.innerHTML + messageHtml;
 }
